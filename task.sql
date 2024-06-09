00c15d5d72b3a3411b88a2bd6de6f1aba94ffe43
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-06-10 00:05:24 +0300

    Solution_v1.0

--- task.sql
+++ task.sql
@@ -1,11 +1,14 @@
 -- Create database and tables
 
 CREATE DATABASE ShopDB;
+
 USE ShopDB;
 
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
+    WarehouseName VARCHAR(50),
+    WarehouseAddress VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
@@ -13,21 +16,55 @@ CREATE TABLE ProductInventory (
     ID INT,
     ProductName VARCHAR(50),
     WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
     CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (CountryID) REFERENCES Countries (ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
 -- Populate test data
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO
+    Countries (
+        ID,
+        Name,
+        WarehouseName,
+        WarehouseAddress
+    )
+VALUES (
+        1,
+        'Country1',
+        'Warehouse-1',
+        'City-1, Street-1'
+    );
+
+INSERT INTO
+    Countries (
+        ID,
+        Name,
+        WarehouseName,
+        WarehouseAddress
+    )
+VALUES (
+        2,
+        'Country2',
+        'Warehouse-2',
+        'City-2, Street-2'
+    );
+
+INSERT INTO
+    ProductInventory (
+        ID,
+        ProductName,
+        WarehouseAmount,
+        CountryID
+    )
+VALUES (1, 'AwersomeProduct', 2, 1);
+
+INSERT INTO
+    ProductInventory (
+        ID,
+        ProductName,
+        WarehouseAmount,
+        CountryID
+    )
+VALUES (2, 'AwersomeProduct', 5, 2);
